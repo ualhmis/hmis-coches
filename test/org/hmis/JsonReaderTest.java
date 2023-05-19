d79e191571db5fb76a5ce0a3682c26dbbf864ec1
Cristian Gil García <cgg734@inlumine.ual.es>
2023-05-19 20:05:22 +0200

    jsonreader

--- test/org/hmis/JsonReaderTest.java
+++ test/org/hmis/JsonReaderTest.java
@@ -4,8 +4,13 @@ package org.hmis;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.EmptyStackException;
 
 import org.junit.jupiter.api.Test;
+
+import com.google.gson.JsonIOException;
 
 class JsonReaderTest {
 
@@ -32,5 +37,60 @@ class JsonReaderTest {
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
 		assertThrows(ArrayIndexOutOfBoundsException.class, () -> coches[4].equals(null));
 	}
+	
+	@Test
+	public void testLeerCochesJSON_ArchivoVacio() {
+		// Preparación
+		String archivoVacio = "ruta_archivo_vacio.json";
+		crearArchivoVacio(archivoVacio);
+
+		// Ejecución
+		Coche[] coches = JsonReader.leerCochesJSON(archivoVacio);
+
+		// Verificación
+		assertNull(coches);
+	}
+
+	@Test
+	public void testLeerCochesJSON_ArchivoMalformado() {
+		// Preparación
+		String archivoMalformado = "ruta_archivo_malformado.json";
+		crearArchivoMalformado(archivoMalformado);
+
+		// Ejecución
+		Coche[] coches = JsonReader.leerCochesJSON(archivoMalformado);
+
+		// Verificación
+		assertNull(coches);
+	}
+
+	private void crearArchivoVacio(String ruta) {
+		try {
+			FileWriter writer = new FileWriter(ruta);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void crearArchivoMalformado(String ruta) {
+		try {
+			FileWriter writer = new FileWriter(ruta);
+			writer.write("Esto no es un JSON válido");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+    public void testJsonReaderInstantiation() {
+        // Ejecución
+        JsonReader jsonReader = new JsonReader();
+
+        // Verificación
+        assertNotNull(jsonReader);
+    }
 
 }
